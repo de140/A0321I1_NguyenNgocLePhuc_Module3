703515c1e8d87e2e42b9b65a2f41f8e667d31043
A0321I1-NguyenNgocLePhuc <phucnnlde140045@fpt.edu.vn>
2021-10-03 22:26:40 +0700

    bt b3 e nop lau r.. con b4 la em bo nham sang bai3

--- bai3_thao_tac_voi_csdl/bai_tap/management_database.sql
+++ bai3_thao_tac_voi_csdl/bai_tap/management_database.sql
@@ -1,44 +1,86 @@
-create database management_library;
+CREATE DATABASE library;
+USE library;
 
-CREATE TABLE Category (
-    category_number INT NOT NULL auto_increment PRIMARY KEY,
+CREATE TABLE categories (
+	id INT AUTO_INCREMENT PRIMARY KEY,
     category_name VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE Book (
-    category_number INT NOT NULL,
-    FOREIGN KEY (category_number)
-        REFERENCES Category (category_number),
-    book_number INT NOT NULL PRIMARY KEY,
-    book_name VARCHAR(50) NOT NULL,
-    publisher VARCHAR(50) NOT NULL,
-    author VARCHAR(50) NOT NULL,
-    publisher_year INT NOT NULL
+CREATE TABLE producer (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    producer_name VARCHAR(50) NOT NULL,
+    produce_year YEAR,
+    author_name VARCHAR(50),
+    publish_times INT NOT NULL
 );
 
-CREATE TABLE Student (
-    student_number VARCHAR(10) NOT NULL PRIMARY KEY,
+CREATE TABLE address (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    street VARCHAR(40) NOT NULL,
+    ward VARCHAR(40) NOT NULL,
+    district VARCHAR(40),
+    city_or_province VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE student (
+	id INT AUTO_INCREMENT PRIMARY KEY,
     student_name VARCHAR(50) NOT NULL,
-    address_line VARCHAR(100) NOT NULL,
-    city VARCHAR(50) ,
-    state VARCHAR(50),
-    postcode VARCHAR(10) NOT NULL,
-    email VARCHAR(15) NOT NULL,
-    image VARBINARY(1000)
+    birth_of_date DATE NOT NULL,
+    book_name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    phone VARCHAR(15) NOT NULL,
+    image VARCHAR(255),
+    address_id INT,
+    FOREIGN KEY (address_id) REFERENCES address(id)
 );
 
-CREATE TABLE BorrowOrder (
-    borrow_number VARCHAR(10) NOT NULL PRIMARY KEY,
-    student_number VARCHAR(10) NOT NULL,
-    FOREIGN KEY (student_number)
-        REFERENCES Student (student_number),
-    book_number INT NOT NULL,
-    FOREIGN KEY (book_number)
-        REFERENCES Book (book_number),
-    borrow_date DATETIME NOT NULL,
-    return_date DATE NOT NULL
+CREATE TABLE library_card (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    student_id INT,
+    FOREIGN KEY (student_id) REFERENCES student(id)
 );
 
+CREATE TABLE book (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    price DOUBLE NOT NULL,
+    producer_id INT,
+    category_id INT,
+    FOREIGN KEY (producer_id) REFERENCES producer(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+CREATE TABLE librarian (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    librarian_name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    phone VARCHAR(15) NOT NULL,
+    address_id INT,
+    FOREIGN KEY (address_id) REFERENCES address(id)
+);
+
+CREATE TABLE register_to_borrow (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    register_date DATE NOT NULL,
+    book_id INT,
+    student_id INT,
+    FOREIGN KEY (book_id) REFERENCES book(id),
+    FOREIGN KEY (student_id) REFERENCES student(id)
+);
+
+CREATE TABLE slip_borrow (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    date_borrow DATE NOT NULL,
+    book_return_date DATE NOT NULL, 
+    CHECK (book_return_date >= date_borrow),
+    actual_return_date DATE NOT NULL,
+    CHECK (actual_return_date <= book_return_date),
+    student_id INT,
+    book_id INT,
+    librarian_id INT,
+    FOREIGN KEY (student_id) REFERENCES student(id),
+    FOREIGN KEY (book_id) REFERENCES book(id),
+    FOREIGN KEY (librarian_id) REFERENCES librarian(id)
+);
 
 
 
